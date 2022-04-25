0a23af23d8f077d13a0e3fbc0e7d42193d2f5625
anhducleanning <haanhduc.lienvietttech@gmail.com>
2022-04-25 14:13:07 +0700

    ngFor - HTML - CSS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Anh Đức';
   public age = 20 ;
+  public nameSmartPhone = ['Apple','SamSung','Xiaomi','Nokia','Google','Oppo'];
+  public priceSmarPhone = ['1000','2000','450','656','546','225'];
+
+  public nameSPObject = [
+    { ten : 'Apple',gia : 1000, hagia :true},
+    {ten :'SamSung',gia : -200,hagia :false},
+    {ten :'Xiaomi',gia : 423,hagia :true},
+    {ten :'Nokia',gia : 545,hagia :false},
+    {ten :'Google',gia : - 213,hagia :true},
+    {ten :'Oppo',gia : 518,hagia :false},
+]
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public resetName(): void {
-    console.log("resetName")
-      this.name = 'Anh Đức';
-  }
 
 }
